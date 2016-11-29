402d51d9f7274a602941b02680e33e05b42b6e78
aude <aude.wiki@gmail.com>
2016-11-28 20:40:24 -0500

    Remove old code

--- src/CiteTool.js
+++ src/CiteTool.js
@@ -1,68 +1,5 @@
 ( function( wb, mw, $ ) {
 
-var CiteTool = {
-
-	init: function( options ) {
-		this.options = $.extend( {},
-			this.options,
-			options
-		);
-	},
-
-	makeLink: function() {
-		var self = this;
-
-		var iconWidget = new OO.ui.IconWidget( {
-			icon: 'search',
-			iconTitle: 'Lookup'
-		} );
-
-		iconWidget.$element
-			.on( 'click', function( e ) {
-				e.preventDefault();
-
-				$( '.wikibase-referenceview-new' ).css( { 'display': 'none' } );
-
-				var $statement = e.target.closest( '.wikibase-statementview' ),
-					classes = $statement.getAttribute( 'class' ).split( ' ' ),
-					guid = null;
-
-				$.each( classes, function( key, value ) {
-					if ( value.match( /wikibase-statement-Q/i ) ) {
-						guid = value.substring( 19 );
-					}
-				} );
-
-				if ( guid !== null ) {
-					$.ajax({
-						url: self.options.templateUrl,
-						dataType: 'json',
-						success: function( template ) {
-							var windowManager = new OO.ui.WindowManager();
-
-							$( 'body' ).append( windowManager.$element );
-
-							var referenceDialog = new ReferenceDialog(
-								template,
-								guid,
-								mw.config.get( 'wgRevisionId' ),
-								{
-									size: 'large'
-								}
-							);
-
-							windowManager.addWindows( [ referenceDialog ] );
-							windowManager.openWindow( referenceDialog );
-						}
-					});
-				}
-			} );
-
-		return iconWidget.$element;
-	}
-
-};
-
 CiteToolSnakBuilder = {
 
 	snakData: {},
@@ -115,257 +52,17 @@ CiteToolSnakBuilder = {
 
 };
 
-function FormPanelView() {
-
-	var lookupInput;
-
-	var render = function() {
-		var panel = new OO.ui.PanelLayout( {
-			padded: true,
-			expanded: false,
-			classes: [ 'refdialog-panel-lookup' ]
-		} );
-
-		var fieldSetLayout = new OO.ui.FieldsetLayout();
-
-		lookupInput = new OO.ui.TextInputWidget( {
-			placeholder: 'Enter url'
-		} );
-
-		fieldSetLayout.addItems( [
-			new OO.ui.FieldLayout(
-				lookupInput,
-				{
-					label: 'URL'
-				}
-			)
-		] );
-
-		panel.$element.append( fieldSetLayout.$element );
-		panel.$element.addClass( 'wikidata-refs-ReferencesWidget' );
-
-		return panel;
-	};
-
-	var getLookupInputValue = function() {
-		return lookupInput.getValue();
-	};
-
-	return {
-		render: render,
-		getLookupInputValue: getLookupInputValue
-	};
-
-}
-
-function ResultsPanelView() {
-
-	var resultsPanel;
-
-	var render = function() {
-		resultsPanel = new OO.ui.PanelLayout( {
-			$content: $( '<h3>Result</h3>' ),
-			classes: [ 'refdialog-panel-result' ],
-			padded: true,
-			scrollable: true,
-			expanded: false
-		} );
-
-		return resultsPanel;
-	};
-
-	var appendResult = function( $result ) {
-		resultsPanel.$element.append( $result );
-	};
-
-	return {
-		appendResult: appendResult,
-		render: render
-	};
-
-}
-
-ReferenceDialogLoader = {};
-
-function ReferenceDialog( template, guid, baseRevId, config ) {
-	ReferenceDialog.super.call( this, config );
-
-	this.template = template;
-	this.guid = guid;
-	this.baseRevId = baseRevId;
-
-	this.formPanelView = new FormPanelView();
-	this.resultsPanelView = new ResultsPanelView();
-}
-
-OO.inheritClass( ReferenceDialog, OO.ui.ProcessDialog );
-
-ReferenceDialog.static.title = 'Add a reference';
-ReferenceDialog.static.actions = [
-	{
-		action: 'lookup',
-		label: 'Lookup',
-		flags: [ 'primary', 'constructive' ],
-		modes: [ 'lookup' ]
-	},
-	{
-		action: 'save',
-		label: 'Save',
-		flags: [ 'primary', 'constructive' ],
-		modes: [ 'result' ]
-	},
-	{
-		label: 'Cancel',
-		flags: 'safe',
-		modes: [ 'lookup', 'result' ]
-	}
-];
-
-ReferenceDialog.prototype.initialize = function() {
-	ReferenceDialog.super.prototype.initialize.apply( this, arguments );
-
-	this.citoidData = null;
-	this.snakData = {};
-
-	var panel = this.formPanelView.render(),
-		resultsPanel = this.resultsPanelView.render();
-
-	this.panels = {
-		lookup: panel,
-		result: resultsPanel
-	};
-
-	this.stack = new OO.ui.StackLayout( {
-		items: [ panel, resultsPanel ],
-		classes: [ 'container' ],
-		expanded: false
-	} );
-
-	this.$body.append( this.stack.$element );
-};
-
-ReferenceDialog.prototype.getSetupProcess = function( data ) {
-	data = data || {};
-
-	return ReferenceDialog.super.prototype.getSetupProcess.call( this, data )
-		.next( function() {
-			this.setModePanel( 'lookup' );
-			this.actions.setMode( 'lookup' );
-		}, this );
-};
-
-ReferenceDialog.prototype.getActionProcess = function( action ) {
-	var self = this;
-
-	if ( action === 'lookup' ) {
-		return new OO.ui.Process( function () {
-			return self.doLookup( self.formPanelView.getLookupInputValue() );
-		} );
-	} else if ( action === 'save' ) {
-		return new OO.ui.Process( function() {
-			return self.saveReference();
-		} );
-	}
-
-	return ReferenceDialog.super.prototype.getActionProcess.call( this, action );
-};
-
-ReferenceDialog.prototype.setModePanel = function( panelName ) {
-	this.stack.setItem( this.panels[panelName] );
-};
-
-ReferenceDialog.prototype.doLookup = function( urlValue ) {
-	var self = this;
-
-	return $.ajax( {
-		method: 'GET',
-		url: 'https://citoid.wikimedia.org/api',
-		data: {
-			action: 'query',
-			format: 'mediawiki',
-			search: urlValue,
-			basefields: true,
-			jsonp: true
-		}
-	} )
-	.then( function( citoidData ) {
-		self.data = citoidData,
-
-		self.snakBuilder = Object.create( CiteToolSnakBuilder ),
-		self.snakBuilder.init( {
-			userLanguage: mw.config.get( 'wgUserLanguage' )
-		} );
-
-		$.each( citoidData[0], function( key, value ) {
-			if ( !self.template[key] ) {
-				return;
-			}
-
-			var entityId = self.template[key].id;
-
-			self.$resultTable = $( '<table>' )
-				.attr( { 'id': 'wikidata-citetool-results' } );
-
-			self.lookupLabel( entityId ).done( function( labelData ) {
-				self.snakBuilder.setSnakValue( self.template[key], value );
-
-				var $row = $( '<tr>' ),
-					$propertyCell = $( '<td>' ).text( labelData.entities[entityId].labels.en.value ),
-					$valueCell = $( '<td>' ).text( value );
-
-				$row.append( $propertyCell );
-				$row.append( $valueCell );
-
-				self.$resultTable.append( $row );
-
-				self.resultsPanelView.appendResult( self.$resultTable );
-
-				self.actions.setMode( 'result' );
-				self.setModePanel( 'result' );
-				self.updateSize();
-			} );
-		} );
-	} );
-};
-
-ReferenceDialog.prototype.saveReference = function() {
-	var api = new wb.api.RepoApi( new mw.Api() );
-
-	return api.setReference( this.guid, this.snakBuilder.getSnakData(), this.baseRevId )
-		.then( function( res ) {
-			console.log( res );
-		} );
-};
-
-ReferenceDialog.prototype.lookupLabel = function( entityIds ) {
-	var baseURI = '/w/api.php',
-		params = {
-			action: 'wbgetentities',
-			ids: entityIds,
-			props: 'labels',
-			format: 'json'
-		};
-
-	return $.ajax( {
-		url: baseURI,
-		data: params,
-		dataType: 'jsonp',
-		jsonp: 'callback'
-	} );
-};
-
-
-function CiteToolController() {
+function CiteTool() {
 	var self = this;
 };
 
-CiteToolController.prototype.getReferenceFromView = function( referenceView ) {
+CiteTool.prototype.getReferenceFromView = function( referenceView ) {
 	var refView = $( referenceView ).data( 'referenceview' );
 
 	return refView.value();
 };
 
-CiteToolController.prototype.hasLookupSnakProperty = function( reference ) {
+CiteTool.prototype.hasLookupSnakProperty = function( reference ) {
 	var hasLookupSnak = false,
 		snaks = reference.getSnaks();
 
@@ -378,7 +75,7 @@ CiteToolController.prototype.hasLookupSnakProperty = function( reference ) {
 	return hasLookupSnak;
 };
 
-CiteToolController.prototype.getLookupSnakValue = function( reference ) {
+CiteTool.prototype.getLookupSnakValue = function( reference ) {
 	var value = null;
 
 	reference.getSnaks().each( function( k, snak ) {
@@ -390,7 +87,7 @@ CiteToolController.prototype.getLookupSnakValue = function( reference ) {
 	return value;
 };
 
-CiteToolController.prototype.onAutofillClick = function( target ) {
+CiteTool.prototype.onAutofillClick = function( target ) {
 	var self = this,
 		referenceView = $( target ).closest( '.wikibase-referenceview' ),
 		reference = this.getReferenceFromView( referenceView );
@@ -427,7 +124,7 @@ CiteToolController.prototype.onAutofillClick = function( target ) {
 		} );
 };
 
-CiteToolController.prototype.addAutofillLink = function( referenceView ) {
+CiteTool.prototype.addAutofillLink = function( referenceView ) {
 	var self = this;
 
 	$( referenceView ).find( '.wikibase-referenceview-heading' ).append(
@@ -440,7 +137,7 @@ CiteToolController.prototype.addAutofillLink = function( referenceView ) {
 		);
 };
 
-CiteToolController.prototype.doLookup = function( value ) {
+CiteTool.prototype.doLookup = function( value ) {
 	var dfd = $.Deferred();
 
 	$.ajax( {
@@ -478,11 +175,11 @@ ReferenceDialogLoader = {
 						return;
 					}
 
-					var citeToolController = new CiteToolController(),
-						reference = citeToolController.getReferenceFromView( e.target );
+					var citeTool = new CiteTool(),
+						reference = citeTool.getReferenceFromView( e.target );
 
-					if ( reference && citeToolController.hasLookupSnakProperty( reference ) ) {
-						citeToolController.addAutofillLink( e.target );
+					if ( reference && citeTool.hasLookupSnakProperty( reference ) ) {
+						citeTool.addAutofillLink( e.target );
 					}
 				} );
 		}, 300 );
