d2a3acd696c10d04a58461cab09132c55f0f533f
Marielle Volz <marielle.volz@gmail.com>
2018-10-02 14:59:05 +0100

    Update -doc.js

--- src/-doc.js
+++ src/-doc.js
@@ -3,7 +3,7 @@
 
  mw.loader.using(['wikibase'], function() {
 	$.getScript( 'https://www.wikidata.org/w/index.php?title=User:Aude/CiteTool.js&action=raw&ctype=text/javascript', function() {
-		var citeTool = new wb.CiteTool( 'https://www.wikidata.org/w/index.php?title=User:Aude/CiteProperties.json&action=raw&ctype=text/javascript' );
+		var citeTool = new wb.CiteTool( 'https://www.wikidata.org/w/index.php?title=User:Aude/CiteProperties.json&action=raw&ctype=application/json' );
 		citeTool.init();
 	});
 });
